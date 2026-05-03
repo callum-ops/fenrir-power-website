6501e93a281797f40132072c858c1d139a9604e5
callum-ops <callum@fenrirpower.com>
2026-05-03 15:40:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ export default function App() {
     backgroundImage: "url('/turbine.jpg')",
     backgroundSize: "cover",
     backgroundPosition: "center",
-    filter: "grayscale(80%) brightness(0.60)",
+    filter: "grayscale(50%) brightness(0.20)",
     transform: "scale(1.03)",
     zIndex: 0
   }} />
